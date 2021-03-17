5a7d5b7154eb7c5caaea39f5b02ea526c1adf2ba
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-03-17 08:09:22 +0700

    Swipeable Tabs

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import {StyleSheet, View, Text, Image, ScrollView} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  ScrollView,
+  Dimensions,
+  useWindowDimensions,
+} from 'react-native';
 import {ProfileDummy} from '../../assets';
 import {ItemsCard} from '../../components';
 import Gap from './../../components/atoms/Gap';
+import {TabView, SceneMap} from 'react-native-tab-view';
+
+const FirstRoute = () => <View style={{flex: 1, backgroundColor: '#ff4081'}} />;
+
+const SecondRoute = () => (
+  <View style={{flex: 1, backgroundColor: '#673ab7'}} />
+);
 
 function Home() {
+  const layout = useWindowDimensions();
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {key: '1', title: 'New Items'},
+    {key: '2', title: 'Popular'},
+    {key: '3', title: 'Recommended'},
+  ]);
+
+  const renderScene = SceneMap({
+    1: FirstRoute,
+    2: SecondRoute,
+    3: FirstRoute,
+  });
+
   return (
-    <View>
+    <View style={styles.pages}>
       <View style={styles.profileContainer}>
         <View>
           <Text style={styles.appName}>ShopAroundMe</Text>
@@ -14,14 +43,24 @@ function Home() {
         </View>
         <Image source={ProfileDummy} style={styles.profile} />
       </View>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        <View style={styles.itemCardContainer}>
-          <ItemsCard />
-          <ItemsCard />
-          <ItemsCard />
-          <ItemsCard />
-        </View>
-      </ScrollView>
+      <View>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <View style={styles.itemCardContainer}>
+            <ItemsCard />
+            <ItemsCard />
+            <ItemsCard />
+            <ItemsCard />
+          </View>
+        </ScrollView>
+      </View>
+      <View style={styles.itemsTabsContainer}>
+        <TabView
+          navigationState={{index, routes}}
+          renderScene={renderScene}
+          onIndexChange={setIndex}
+          initialLayout={{width: layout.width}}
+        />
+      </View>
     </View>
   );
 }
@@ -40,5 +79,12 @@ const styles = StyleSheet.create({
   appName: {fontSize: 22, fontFamily: 'Poppins-Medium', color: '#020202'},
   desc: {fontSize: 14, fontFamily: 'Poppins-Light', color: '#8D92A3'},
   profile: {width: 50, height: 50, borderRadius: 8},
-  itemCardContainer: {flexDirection: 'row', marginVertical: 24},
+  itemCardContainer: {flexDirection: 'row', marginVertical: 24, marginLeft: 24},
+  pages: {
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+  itemsTabsContainer: {
+    flex: 1,
+  },
 });
